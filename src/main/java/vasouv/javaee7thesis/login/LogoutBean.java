ceb56992cef7a5019b8299c938f165e1d1a778f8
Souvatzis Vasilios <vasouv@gmail.com>
2014-08-03 00:40:57 +0300

    Initial commit
    Initial code from https://github.com/jlombardo/jsfsecure

---
+++ src/main/java/vasouv/javaee7thesis/login/LogoutBean.java
@@ -0,0 +1,50 @@
+package vasouv.javaee7thesis.login;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * This JSF/CDI Managed Bean provides a way for users to log out of the
+ * application.
+ */
+@Named(value = "logoutBean")
+@RequestScoped
+public class LogoutBean {
+    // Built in logging is used now, but will be replaced in future with
+    // 3rd party solution. Logging does what System.out.println() does and
+    // more!
+    private static Logger log = Logger.getLogger(LogoutBean.class.getName());
+
+    public String logout() {
+        // Notice the redirect syntax. The forward slash means start at
+        // the root of the web application.
+        String destination = "/index?faces-redirect=true";
+
+        // FacesContext provides access to other container managed objects,
+        // such as the HttpServletRequest object, which is needed to perform
+        // the logout operation.
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = 
+                (HttpServletRequest) context.getExternalContext().getRequest();
+
+        try {
+            // added May 12, 2014
+            HttpSession session = request.getSession();
+            session.invalidate();
+            
+            // this does not invalidate the session but does null out the user Principle
+            request.logout();
+        } catch (ServletException e) {
+            log.log(Level.SEVERE, "Failed to logout user!", e);
+            destination = "/loginerror?faces-redirect=true";
+        }
+
+        return destination; // go to destination
+    }
+}
---
+++ src/main/sql/createDB.sql
@@ -0,0 +1,22 @@
+create database jdbcrealm;
+
+create table users (
+  id int not null primary key,
+  username varchar(255) unique not null,
+  password varchar(255) unique not null,
+  name varchar(255) not null,
+  email varchar(255) not null
+);
+
+create table groups (
+  username varchar(255) not null primary key,
+  groupname varchar(255) not null
+);
+
+insert into users values (1,'vasouv','vasouv','Vasilis','vasouv@admin.com');
+insert into users values (2,'geo','geo','George','geo@admin.com');
+insert into users values (3,'maik','maik','Mixalis','maik@maik.com');
+
+insert into groups values ('vasouv','admin');
+insert into groups values ('geo','admin');
+insert into groups values ('maik','user');

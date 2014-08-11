e4d3392c5164b6cdd54287c35a6eb500e9405c89
Souvatzis Vasilios <vasouv@gmail.com>
2014-08-11 16:00:57 +0300

    Adding JSF Beans and EJB

--- src/main/java/vasouv/javaee7thesis/register/RegisterUserJSFBean.java
+++ src/main/java/vasouv/javaee7thesis/register/RegisterUserJSFBean.java
@@ -26,6 +26,60 @@ public class RegisterUserJSFBean implements Serializable {
      * Creates a new instance of RegisterUserManagedBean
      */
     public RegisterUserJSFBean() {
+        user = new User();
+        group = new Groups();
+    }
+    
+    /**
+     * GETTERS AND SETTERS
+     */
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Groups getGroup() {
+        return group;
+    }
+
+    public void setGroup(Groups group) {
+        this.group = group;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
     
 }

a6a517258a27157173977be39034e148f61a111c
Souvatzis Vasilios <vasouv@gmail.com>
2014-08-20 02:24:16 +0300

    Some derping with Netbeans git support

---
+++ target/generated-sources/annotations/vasouv/javaee7thesis/register/Groups_.java
@@ -0,0 +1,14 @@
+package vasouv.javaee7thesis.register;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-08-20T02:02:25")
+@StaticMetamodel(Groups.class)
+public class Groups_ { 
+
+    public static volatile SingularAttribute<Groups, String> groupname;
+    public static volatile SingularAttribute<Groups, String> username;
+
+}
---
+++ target/generated-sources/annotations/vasouv/javaee7thesis/register/User_.java
@@ -0,0 +1,17 @@
+package vasouv.javaee7thesis.register;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-08-20T02:02:25")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> password;
+    public static volatile SingularAttribute<User, String> name;
+    public static volatile SingularAttribute<User, Integer> id;
+    public static volatile SingularAttribute<User, String> email;
+    public static volatile SingularAttribute<User, String> username;
+
+}

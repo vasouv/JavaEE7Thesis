504410033bb50b1dfcbb6ca9c42ff46b47f53361
Souvatzis Vasilios <vasouv@gmail.com>
2014-08-23 18:34:01 +0300

    Basic logging when creating new User

--- src/main/java/vasouv/javaee7thesis/register/sessionbeans/GroupsFacade.java
+++ src/main/java/vasouv/javaee7thesis/register/sessionbeans/GroupsFacade.java
@@ -6,6 +6,8 @@
 
 package vasouv.javaee7thesis.register.sessionbeans;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -20,17 +22,28 @@ public class GroupsFacade extends AbstractFacade<Groups> {
     @PersistenceContext(unitName = "vasouvPU")
     private EntityManager em;
     
+    //This logger is used to console print when a group is created
+    private static final Logger glog = Logger.getLogger("groupLogger");
+    
     /**
      * Persists the Group to the database.
      * 
      * The Group argument is persisted to the database, after its id property
      * is set to the corresponding id to be inserted.
      * 
+     * Logs to the console the username and group.
+     * 
      * @param g The Group to be persisted to the database.
      */
     @Override
     public void create(Groups g) {
+        
+        //Persists to the DB
         em.persist(g);
+        
+        //Logs to the console the username and group that was persisted to the DB
+        glog.log(Level.INFO,"Username {0}" + " " + "was added in group: {1}", 
+                new Object[]{g.getUsername(), g.getGroupname()});
     }
 
     @Override

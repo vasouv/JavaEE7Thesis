70279a3d9ee2a7bfc60d3a1b0746a7a7b2eb4229
Souvatzis Vasilios <vasouv@gmail.com>
2014-10-21 12:40:40 +0300

    Success with managed beans login

---
+++ src/main/java/vasouv/javaee7thesis/login/AuthenticationController.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vasouv.javaee7thesis.login;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import vasouv.javaee7thesis.register.User;
+
+/**
+ *
+ * @author vasouv
+ */
+@Named(value = "authenticationJSFBean")
+@SessionScoped
+public class AuthenticationController implements Serializable {
+
+    @EJB
+    private AuthenticationEJB authenticationFacade;
+    private String username;
+    private User user;
+    private boolean authenticated = false;
+    private HttpSession session = null;
+    private String userAgent;
+
+    /**
+     * Creates a new instance of AuthenticationController
+     */
+    public AuthenticationController() {
+    }
+
+    public HttpSession getSession() {
+        // if(session == null){
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        session = request.getSession();
+
+        return session;
+    }
+    
+    public String login() {
+        authenticationFacade.setUser(getUser());
+        boolean authResult = authenticationFacade.login();
+
+        if (authResult) {
+            this.authenticated = true;
+
+            setUser(authenticationFacade.getUser());
+
+            return "users/main.xhtml";
+        } else {
+            this.authenticated = false;
+            setUser(null);
+            return "loginerror";
+        }
+
+    }
+
+    public String logout() {
+        user = null;
+        this.authenticated = false;
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        externalContext.invalidateSession();
+        return "logout";
+    }
+
+    /**
+     * @return the authenticated
+     */
+    public boolean isAuthenticated() {
+        try {
+            boolean auth = (Boolean) getSession().getAttribute("authenticated");
+            if (auth) {
+                this.authenticated = true;
+
+            } else {
+                authenticated = false;
+            }
+        } catch (Exception e) {
+            this.authenticated = false;
+        }
+
+        return authenticated;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        this.username = getUser().getUsername();
+        return this.username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+        getUser().setUsername(username);
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return authenticationFacade.getPassword();
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        authenticationFacade.setPassword(password);
+    }
+
+    public User getUser() {
+        if (this.user == null) {
+            user = new User();
+            setUser(authenticationFacade.getUser());
+        }
+
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    
+
+    public void setAuthenticated(boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+
+}

2a507074c0c8a6c5cdd8514f49b079b35e7743ba
Souvatzis Vasilios <vasouv@gmail.com>
2014-12-29 12:39:28 +0200

    If username exists on registration, throw message.

--- src/main/java/vasouv/javaee7thesis/register/RegisterUserJSFBean.java
+++ src/main/java/vasouv/javaee7thesis/register/RegisterUserJSFBean.java
@@ -3,6 +3,8 @@ package vasouv.javaee7thesis.register;
 import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.validation.constraints.Pattern;
 import vasouv.javaee7thesis.register.sessionbeans.GroupsFacade;
@@ -29,7 +31,7 @@ public class RegisterUserJSFBean implements Serializable {
     private String username;
     private String password;
     private String name;
-    
+
     //Validation pattern taken from Josh Juneau's JavaEE 7 Recipes, ch. 5-4, Solution 2
     @Pattern(regexp = "[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+", message = "Email format is invalid. Must be sample@mail.com")
     private String email;
@@ -41,21 +43,43 @@ public class RegisterUserJSFBean implements Serializable {
 
     /**
      * Registers the new user and navigates to the registersuccess webpage.
+     * 
+     * The username field checks the DB for the given value. If the username exists,
+     * it throws a message. Otherwise, the registration completes.
      *
      * @return the webpage for successful registration
      */
     public String registerNewUser() {
-        user.setName(this.name);
-        user.setEmail(this.email);
-        user.setUsername(this.username);
-        user.setPassword(this.password);
-        group.setUsername(this.username);
-        group.setGroupname("user");
+        String webpage;
+        if (checkIfUsernamePasswordExist()) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Username already exists", ""));
+            webpage = "/register.xhtml";
+        } else {
+            user.setName(this.name);
+            user.setEmail(this.email);
+            user.setUsername(this.username);
+            user.setPassword(this.password);
+            group.setUsername(this.username);
+            group.setGroupname("user");
 
-        userEJB.create(user);
-        groupEJB.create(group);
+            userEJB.create(user);
+            groupEJB.create(group);
+            
+            webpage = "/registersuccess.xhtml";
+        }
 
-        return "/registersuccess.xhtml";
+        return webpage;
+    }
+
+    /**
+     * Checks if username exists.
+     * 
+     * Helper method that checks if the username already exists in the DB.
+     * 
+     * @return true or false, if the username exists.
+     */
+    private boolean checkIfUsernamePasswordExist() {
+        return userEJB.usernameExists(username);
     }
 
     //GETTERS & SETTERS
